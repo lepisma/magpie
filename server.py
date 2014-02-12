ed1251f2c7690e1e92d23dc987cf86ca1c65caf0
Abhishek Kandoi <abhikandoi2000@gmail.com>
2014-02-13 00:29:31 +0530

    Adds route for power
    * Automatically created sqlite3 db and table

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from bottle import Bottle, request, response, run, template, static_file
 import writer
+import sqlite3
+import json
 
 app = Bottle()
 
@@ -20,6 +22,31 @@ def mainpage():
 @app.get('<path:path>')
 def server_public(path):
 	return static_file(path, root = 'public/')
+
+@app.get('/power')
+def power_data():
+  """Returns  power data in JSON format
+  """
+  response.headers['Content-Type'] = 'application/json'
+
+  connection = sqlite3.connect("app/models/magpie.db")
+  cursor = connection.cursor()
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS power
+                    (date text, power real)
+                 """)
+
+  # insert sample data
+  cursor.execute("INSERT INTO power VALUES ('14/02/2014', 6.05)")
+  cursor.execute("INSERT INTO power VALUES ('15/02/2014', 5.64)")
+
+  # fetch power data
+  cursor.execute("SELECT * FROM power")
+  data = cursor.fetchall()
+
+  dict = {"power_data": data}
+
+  return json.dumps(dict)
 
 @app.post('/change')
 def change():
