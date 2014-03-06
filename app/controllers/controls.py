30f3b86c6f884be97cd890a4d62401ecf2b0872e
Akshesh <aksheshdoshi@gmail.com>
2014-03-06 17:41:09 +0530

    send 5 chars in the write function for xbee

--- app/controllers/controls.py
+++ app/controllers/controls.py
@@ -29,20 +29,29 @@ def linkToPi(cursor, route, id, status):
   cursor.execute("SELECT type from switches WHERE id = '" + str(id) + "'")
   switch_type = cursor.fetchall()[0][0]
   switch_id = id[1]
+  switch_id = str(switch_id)
+  if len(switch_id) == 1:
+  	switch_id = "0" + switch_id
   print switch_type
   if route == 'switch':
     if status == "off":
-      print "raspberry.write( " + switch_type + ", " + switch_id + ", " +  "0)"
-      # raspberry.write(switch_type, switch_id, 0)
+      print "raspberry.write( " + switch_type + ", " + switch_id + ", " +  "00)"
+      # raspberry.write(switch_type, switch_id, "00")
     elif switch_type == "F":    # else the switch has been turned on
       cursor.execute("SELECT slide from switches WHERE id = '" + str(id) + "'")
       switch_status = cursor.fetchall()[0][0]
+      switch_status = str(switch_status)
+      if len(switch_status) == 1:
+      	switch_status = "0" + switch_status
       print switch_status
-      print "raspberry.write( " + switch_type + ", " + switch_id + ", " +  str(switch_status) + ")"
-      # raspberry.write(switch_type, switch_id, slide)
+      print "raspberry.write( " + switch_type + ", " + switch_id + ", " + switch_status + ")"
+      # raspberry.write(switch_type, switch_id, switch_status)
     else:
-      print "raspberry.write( " + switch_type + ", " + switch_id + ", " + "1)"
-      # raspberry.write(switch_type, switch_id, 1)
+      print "raspberry.write( " + switch_type + ", " + switch_id + ", " + "01)"
+      # raspberry.write(switch_type, switch_id, "01")
   elif route == 'slide':
-    print "raspberry.write( " + switch_type + ", " + switch_id + ", " +  status + ")"
+  	status = str(status)
+	if len(status) == 1:
+		status = "0" + status
+  	print "raspberry.write( " + switch_type + ", " + switch_id + ", " +  status + ")"
     # raspberry.write(switch_type, switch_id, status)
